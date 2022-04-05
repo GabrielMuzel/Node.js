ca8449fed387bd0c921771ad8af7f90f5e458e15
Gabriel Muzel <100106572+GabrielMuzel@users.noreply.github.com>
2022-04-05 12:43:29 -0300

    Auto_atribuição (node.js) e simplificação !

---
+++ Caculo  Com Auto-atribuição  Node.js
@@ -0,0 +1,28 @@
+> var saldo = 1500
+undefined
+> saldo
+1500
+> saldo *= 2
+3000
+> saldo /= 2
+1500
+> saldo **= 2
+2250000
+> 
+
+
+//Variaveis com auto atribuição  
+> s
+8
+> p
+3
+> v
+45
+> d
+6
+> d2
+2
+> e
+3
+>
+
